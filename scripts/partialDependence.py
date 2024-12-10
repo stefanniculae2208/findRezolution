d911d4335603ca34a379dc8ce1243abbf45cb5c3
stefanniculae2208 <stefan.niculae2208@gmail.com>
2024-12-10 14:35:10 +0200

    my commit

--- scripts/partialDependence.py
+++ scripts/partialDependence.py
@@ -1,35 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from typing import List
 
 
-def findPartialDependence(filename: str):
+def findPartialDependence(
+    filename: str, input_columns: List[str], output_columns: List[str]
+):
 
     data = pd.read_csv(filename)
-
-    print(data)
-
-    input_columns = [
-        "ch_gate",
-        "ch_gateshort",
-        "ch_cfd_delay",
-        "ch_cfd_smoothexp",
-        "ch_cfd_fraction",
-    ]
-    output_columns = ["Count", "Result"]
 
     num_inputs = len(input_columns)
     num_outputs = len(output_columns)
     fig, axes = plt.subplots(num_inputs, num_outputs, figsize=(12, num_inputs * 4))
 
     if num_inputs == 1:
-        axes = [axes]
+        axes = np.array([axes])
+    if num_outputs == 1:
+        axes = axes[:, np.newaxis]
 
     for i, input_col in enumerate(input_columns):
         for j, output_col in enumerate(output_columns):
-            ax = axes[i][j] if num_inputs > 1 else axes[j]
-            ax.scatter(
-                data[input_col], data[output_col], alpha=0.7, label="Data points"
-            )
+
+            # ax = axes[i][j] if num_inputs > 1 else axes[i]
+            ax = axes[i][j]
             ax.plot(
                 data[input_col].sort_values(),
                 data.groupby(input_col)[output_col]
@@ -50,5 +44,15 @@ def findPartialDependence(filename: str):
 
 if __name__ == "__main__":
 
-    filename = "./output/results_alls.csv"
-    findPartialDependence(filename)
+    filename = "./output/results_all.csv"
+    input_columns = [
+        "ch_gate",
+        "ch_gateshort",
+        "ch_cfd_delay",
+        "ch_cfd_smoothexp",
+        "ch_cfd_fraction",
+    ]
+    # output_columns = ["Count", "Result"]
+    output_columns = ["Result"]
+
+    findPartialDependence(filename, input_columns, output_columns)
